bc2a464c6df946334b2f3e5c156ccc03e5772d39
Joel Carlos <joelcarlos02@gmail.com>
2018-10-04 17:29:51 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: path.join(__dirname, "./dist"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: `${__dirname}/`
+  }
+};
